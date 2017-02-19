9efeeb9a502ac5459e522226c7aea10c7fa5884c
Silvio Fragnani da Silva <silviofragnanisilva@gmail.com>
2017-02-19 19:05:10 +0100

    First import

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	
+	public void MovePlayerToPosition(Transform newTransform) {
+		transform.position = newTransform.position;
+	}
+}
---
+++ Assets/Scripts/RoomController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour {
+
+	public GameObject player;
+
+	public void GoToNextRoom(Transform transform) {
+		player.transform.position = transform.position;
+	}
+}
